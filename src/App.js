fbee04ac9ea0e2d89177825587bbbc194eb85cf4
Semih Yonet <semih@shoppingminds.com>
2024-05-29 15:19:11 +0200

    Optimize imports and touch up on Readme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
-import {useState} from 'react'
-import {Dialog, DialogPanel} from '@headlessui/react'
-import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline'
 
-const navigation = []
 
 function App() {
 

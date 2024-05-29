d2d1785ff94f143873e7c15764e26d09597d3847
Semih Yonet <semih@shoppingminds.com>
2024-05-29 16:27:00 +0200

    Fix Icon size

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
                 <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
                      aria-label="Global">
                     <div className="flex lg:flex-1">
-                        <a href="#" className="-m-1.5 p-1.5">
+                        <a href="/" className="-m-1.5 p-1.5">
                             <span className="sr-only">Havegan</span>
                             <img
                                 className="h-16 w-auto"
